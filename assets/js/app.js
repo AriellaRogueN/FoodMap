94b40d1b862e5a377ff8c073dac3b66f69d62efe
AriellaRogueN <arrodriguezn@gmail.com>
2017-12-13 13:01:02 -0300

    Primer commit

---
+++ assets/js/app.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('#chilena').click(function() {
+       
+        $('img').show().filter(':not(#chilean)').hide();
+
+    });
+
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
