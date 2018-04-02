7e19df2134137da6b25007a753d6e23467ba3dd4
AriellaRogueN <arrodriguezn@gmail.com>
2018-04-02 13:15:59 -0300

    Agregando Cambios Documento

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,75 +1,153 @@
-$(document).ready(function () {
-/*Funcion para cambiar opacidad de las imagenes al pasar el mouse*/
- $("img").mouseover(function () {
-    $("img").css("opacity", "0.5");
-
-}).mouseout(function () {
-    $('img').css("opacity","1");
-
-});
-
-/*Funcion para mostrar el modal*/     
-    $("#cfood").click(function () {
-
-       $("miModal").show();
+$(document).ready(function() {
+  // Al pasar el mousse por las imagenes tomar valor de alt y mostrarlo como figcaption y darle estilos.
+  $('.images img').mouseover(function() {
+    var info = $(this).attr('alt');
+    $(this).each(function() {
+      $(this).css({
+        'filter': 'brightness(0.4)',
         
- });
- /*funciones para visualizar y ocultar las imágenes, según su clase*/
-    $("#chilena").click(function() {
-       $(".chilean").css("display", "block");
-       $(".others").css("display", "none");
-       $(".fast").css("display", "none");
-       $(".rest").css("display", "none")
+      });
+      $(this).before('<figcaption>' + info + '</figcaption>');
+      $('figcaption').css({
+        'font-size': '30px',
+        'color': '#363636',
+        'font-weight': 'bolder'
+      });// css
+    });// each
+  });// mouseover
+
+  $('.images img').mouseout(function() {
+    $('.images img').css({
+      'filter': 'brightness(1)',
     });
-    $("#extranjera").click(function () {
-        $(".others").css("display", "block");
-        $(".chilean").css("display", "none");
-        $(".fast").css("display", "none");
-        $(".rest").css("display", "none");
-        
-    });
-    $("#rapida").click(function () {
-        $(".fast").css("display", "block");
-        $(".chilean").css("display", "none");
-        $(".rest").css("display", "none");
-        $(".others").css("display", "none");
+    $('figcaption').remove();
+  });// mouseout
+  
+  var chilena = data.chilena.datos;
+  var extranjera = data.extranjera.datos;
+  var rapida = data.rapida.datos;
+  var restoBar = data.bar.datos;
 
-    });
-    $("#resto-bar").click(function () {
-        $(".rest").css("display", "block");
-        $(".chilean").css("display", "none");
-        $(".fast").css("display", "none");
-        $(".others").css("display", "none");
-    });
-    
+  // Al hacer click en el boton que se muestren las imagenes e info, de la categoria seleccionada.
 
-    
-});
+  $('#img1').click(function() {
+    for (var i = 0; i < chilena.length; i++) {
+      $('.extranjera, .rapida, .resto-bar, .all').empty();
 
+      $('.chilena').append('<div class=\'row\'>' +
+                '<div class=\'col-md-6 text-center newImg\'>' + '<img id=\'chileanPictures\' class=\'Img\' width=\'200\' height=\'200\' src=\'assets/' + chilena[i].picture + '\'>' + '</div>' + 
+                '<div class=\'col-md-6\'>' + 
+                '<h3>' + chilena[i].name + '</h3>' + 
+                '<h4>Dirección: ' + chilena[i].direccion + '</h4>' +
+                '<h4>Telefono: ' + chilena[i].telefono + '</h4>' +
+                '<hr>' +
+                '</div></div>');
+    }
+  });
 
-    
+  $('#img2').click(function() {
+    for (var i = 0; i < extranjera.length; i++) {
+      $('.chilena, .rapida, .resto-bar, .all').empty();
 
+      $('.extranjera').append('<div class=\'row\'>' +
+                '<div class=\'col-md-6 text-center newImg\'>' + '<img class=\'Img\' width=\'200\' height=\'200\' src=\'assets/' + extranjera[i].picture + '\'>' + '</div>' +
+                '<div class=\'col-md-6\'>' +
+                '<h3>' + extranjera[i].name + '</h3>' +
+                '<h4>Dirección: ' + extranjera[i].direccion + '</h4>' +
+                '<h4>Telefono: ' + extranjera[i].telefono + '</h4>' +
+                '<hr>' +
+                '</div></div>');
+    }
+  });
 
+  $('#img3').click(function() {
+    for (var i = 0; i < rapida.length; i++) {
+      $('.chilena, .extranjera, .resto-bar, .all').empty();
 
+      $('.rapida').append('<div class=\'row\'>' +
+                '<div class=\'col-md-6 text-center newImg\'>' + '<img class=\'Img\' width=\'200\' height=\'200\' src=\'assets/' + rapida[i].picture + '\'>' + '</div>' +
+                '<div class=\'col-md-6\'>' +
+                '<h3>' + rapida[i].name + '</h3>' +
+                '<h4>Dirección: ' + rapida[i].direccion + '</h4>' +
+                '<h4>Telefono: ' + rapida[i].telefono + '</h4>' +
+                '<hr>' +
+                '</div></div>');
+    }
+  });
 
+  $('#img4').click(function() {
+    for (var i = 0; i < restoBar.length; i++) {
+      $('.extranjera, .rapida, .chilena, .all').empty();
 
+      $('.resto-bar').append('<div class=\'row\'>' +
+                '<div class=\'col-md-6 text-center newImg\'>' + '<img class=\'Img\' width=\'200\' height=\'200\' src=\'assets/' + restoBar[i].picture + '\'>' + '</div>' +
+                '<div class=\'col-md-6\'>' +
+                '<h3>' + restoBar[i].name + '</h3>' + 
+                '<h4>Dirección: ' + restoBar[i].direccion + '</h4>' +
+                '<h4>Telefono: ' + restoBar[i].telefono + '</h4>' +
+                '<hr>' +
+                '</div></div>');
+    }
+  });
 
+  // Al hacer click en el selector (vista responsive) que se muestren las imagenes e info, de la categoria seleccionada.
+  $('#comida-chilena').click(function() {
+    for (var i = 0; i < chilena.length; i++) {
+      $('.extranjera, .rapida, .resto-bar, .all').empty();
 
+      $('.chilena').append('<div class=\'row\'>' +
+                '<div class=\'col-md-6 text-center\'>' + '<img width=\'200\' height=\'200\' src=\'assets/' + chilena[i].picture + '\'>' + '</div>' +
+                '<div class=\'col-md-6\'>' +
+                '<h3>' + chilena[i].name + '</h3>' + 
+                '<h4>Dirección: ' + chilena[i].direccion + '</h4>' +
+                '<h4>Telefono: ' + chilena[i].telefono + '</h4>' +
+                '<hr/>' +
+                '</div></div>');
+    }
+  });
 
+  $('#comida-extranjera').click(function() {
+    for (var i = 0; i < extranjera.length; i++) {
+      $('.chilena, .rapida, .resto-bar, .all').empty();
 
+      $('.extranjera').append('<div class=\'row\'>' +
+                '<div class=\'col-md-6 text-center\'>' + '<img width=\'200\' height=\'200\' src=\'assets/' + extranjera[i].picture + '\'>' + '</div>' +
+                '<div class=\'col-md-6\'>' +
+                '<h3>' + extranjera[i].name + '</h3>' +
+                '<h4>Dirección: ' + extranjera[i].direccion + '</h4>' +
+                '<h4>Telefono: ' + extranjera[i].telefono + '</h4>' +
+                '<hr/>' +
+                '</div></div>');
+    }
+  });
 
+  $('#comida-rapida').click(function() {
+    for (var i = 0; i < rapida.length; i++) {
+      $('.extranjera, .chilena, .resto-bar, .all').empty();
 
+      $('.rapida').append('<div class=\'row\'>' +
+                '<div class=\'col-md-6 text-center\'>' + '<img width=\'200\' height=\'200\' src=\'assets/' + rapida[i].picture + '\'>' + '</div>' +
+                '<div class=\'col-md-6\'>' +
+                '<h3>' + rapida[i].name + '</h3>' + 
+                '<h4>Dirección: ' + rapida[i].direccion + '</h4>' +
+                '<h4>Telefono: ' + rapida[i].telefono + '</h4>' +
+                '<hr/>' +
+                '</div></div>');
+    }
+  });
 
+  $('#bar-restaurant').click(function() {
+    for (var i = 0; i < restoBar.length; i++) {
+      $('.extranjera, .rapida, .chilena, .all').empty();
 
-
-
-
-
-
-
-
-
-
-
-
-
+      $('.resto-bar').append('<div class=\'rowl\'>' +
+                '<div class=\'col-md-6 text-center\'>' + '<img width=\'200\' height=\'200\' src=\'assets/' + restoBar[i].picture + '\'>' + '</div>' +
+                '<div class=\'col-md-6\'>' +
+                '<h3>' + restoBar[i].name + '</h3>' +
+                '<h4>Dirección: ' + restoBar[i].direccion + '</h4>' +
+                '<h4>Telefono: ' + restoBar[i].telefono + '</h4>' +
+                '<hr/>' +
+                '</div></div>');
+    }
+  });
+});// document
--- assets/js/data.js
+++ assets/js/data.js
@@ -1,121 +1,121 @@
 var data = {
-    'chilena': {
-        'datos': [{
-                'name': 'Restaurant No me olvides',
-                'picture': 'images/chilena/No me olvides.jpeg',
-                'direccion': 'Urriola 609, Rengo, VI Región',
-                'telefono': '(72) 251 3853'
-            },
-            {
-                'name': 'Tucán de Rengo',
-                'picture': 'images/chilena/tucan-h.png',
-                'direccion': 'Avenida la Piscina 6A, Rengo, VI Región',
-                'telefono': '+56 9 76487033'
-            },
-            {
-                'name': 'El rinconcito',
-                'picture': 'images/chilena/rinconcito.JPG',
-                'direccion': 'Libertad 377, Rengo, VI Región',
-                'telefono':  '(72) 514011'
+  'chilena': {
+    'datos': [{
+      'name': 'Restaurant No Me Olvides',
+      'picture': 'images/chilena/No me olvides.jpeg',
+      'direccion': 'Urriola 609, Rengo, VI Región',
+      'telefono': '(72) 251 3853'
+    },
+    {
+      'name': 'Tucán de Rengo',
+      'picture': 'images/chilena/tucan-h.png',
+      'direccion': 'Avenida la Piscina 6A, Rengo, VI Región',
+      'telefono': '+56 9 76487033'
+    },
+    {
+      'name': 'El Rinconcito',
+      'picture': 'images/chilena/rinconcito.JPG',
+      'direccion': 'Libertad 377, Rengo, VI Región',
+      'telefono': '(72) 514011'
 
-            },
-            {
-                'name': 'Adobee',
-                'picture': 'images/chilena/adobee.jpg',
-                'direccion': 'Manuel Rodríguez 198, Rengo, VI Región',
-                'telefono': '(72) 252 9744'
+    },
+    {
+      'name': 'Adobee',
+      'picture': 'images/chilena/adobee.jpg',
+      'direccion': 'Manuel Rodríguez 198, Rengo, VI Región',
+      'telefono': '(72) 252 9744'
                 
-            },
-            {
-                'name': 'Juan y Medio',
-                'picture': 'images/chilena/Juan.jpeg',
-                 'direccion': 'Manuel Rodríguez 198, Rengo, VI Región',
-                 'telefono': ' (72) 252 1726'
-            }
-        ]
     },
-    'extranjera': {
-        'datos': [{
-               'name': 'Mi piace Rengo',
-               'picture': 'images/extranjera/mipiace.jpg',
-               'direccion': 'José Bisquert 284, Rengo, VI Región',
-               'telefono': ' +56 9 8931 8897'
-            },
-            {
-                'name': 'Restaurant Yufa',
-                'picture': 'images/extranjera/yufa.jpg',
-                'direccion': 'Manuel Rodríguez 80, Rengo, VI Región',
-                'telefono': '(72) 251 4099 '
-            },
-            {
-                'name': 'Sushi Sukai',
-                'picture': 'images/extranjera/sukai.jpg',
-                'direccion': 'Arturo Prat 631',
-                'telefono': '+56 9 8312 452'
-            },
-            {
-                'name': 'Sumaq Gastrobar',
-                'picture': 'images/extranjera/sumaq.jpg',
-                'direccion': 'José Bisquert 65, Rengo, VI Región',
-                'telefono': '+56 9 87870941'
-            }
-        ]
-    },
-    'rapida': {
-        'datos': [{
-                'name': 'Chicago Corner Pizza',
-                'picture': 'images/rapida/corner.jpg',
-                'direccion': 'Arturo Prat 754, Rengo, VI Región',
-                'telefono': '+56 9 8122 1297'
-            },
-            {
-                'name': 'Sanguchería el Torreón',
-                'picture': 'images/rapida/eltorreon.png',
-                'direccion': 'Arturo Prat 598, Rengo, VI Región',
-                'telefono': '(72) 276 9694'
-            },
-            {
-                'name': 'Gallo negro',
-                'picture': 'images/rapida/gallo.jpg',
-                'direccion': 'Carlos Condell 219, Rengo, VI Región',
-                'telefono': '+56 9 9694 4988'
-            },
-            {
-                'name': 'Pizzería La Mazza',
-                'picture': 'images/rapida/lamazza.jpg',
-                'direccion': 'José Bisquert 357, Rengo, VI Región',
-                'telefono': '(72) 251 5126'
-            }
-        ]
-    },
-    'bar': {
-        'datos': [{
-                'name': 'Adobee',
-                'picture': 'images/resto-bar/adobee2.jpg',
-                'direccion': 'Manuel Rodríguez 198, Rengo, VI Región',
-                'telefono': ' (72) 252 9744'
-            },
-            {
-                'name': 'El Bar del Camello',
-                'picture': 'images/resto-bar/20245768_302686143587265_2463631577984888387_n.jpg',
-                'direccion': 'Pje 0 2, Rengo, VI Región',
-                'telefono': ' +56 9 89564095'
-            },
-            
-            {
-                'name': 'D-Carlitos',
-                'picture': 'images/resto-bar/carlitos.jpg',
-                'direccion': 'Arturo Prat 503, Rengo, VI Región',
-                'telefono': ' (72) 268 1272'
-            },
-            {
-                'name': 'Sabrosisimo',
-                'picture': 'images/resto-bar/sabro.jpg',
-                'direccion': 'Urriola 119, Rengo, VI Región',
-                'telefono': ' (72) 268 1272'
-            }, 
-        ]
+    {
+      'name': 'Juan y Medio',
+      'picture': 'images/chilena/Juan.jpeg',
+      'direccion': 'Rosario, Rengo, VI Región',
+      'telefono': ' (72) 252 1726'
     }
+    ]
+  },
+  'extranjera': {
+    'datos': [{
+      'name': 'Mi Piace Rengo',
+      'picture': 'images/extranjera/mipiace.jpg',
+      'direccion': 'José Bisquert 284, Rengo, VI Región',
+      'telefono': ' +56 9 8931 8897'
+    },
+    {
+      'name': 'Restaurant Yufa',
+      'picture': 'images/extranjera/yufa.jpg',
+      'direccion': 'Manuel Rodríguez 80, Rengo, VI Región',
+      'telefono': '(72) 251 4099 '
+    },
+    {
+      'name': 'Sushi Sukai',
+      'picture': 'images/extranjera/sukai.jpg',
+      'direccion': 'Arturo Prat 631',
+      'telefono': '+56 9 8312 452'
+    },
+    {
+      'name': 'Sumaq Gastrobar',
+      'picture': 'images/extranjera/sumaq.jpg',
+      'direccion': 'José Bisquert 65, Rengo, VI Región',
+      'telefono': '+56 9 87870941'
+    }
+    ]
+  },
+  'rapida': {
+    'datos': [{
+      'name': 'Chicago Corner Pizza',
+      'picture': 'images/rapida/corner.jpg',
+      'direccion': 'Arturo Prat 754, Rengo, VI Región',
+      'telefono': '+56 9 8122 1297'
+    },
+    {
+      'name': 'Sanguchería el Torreón',
+      'picture': 'images/rapida/eltorreon.png',
+      'direccion': 'Arturo Prat 598, Rengo, VI Región',
+      'telefono': '(72) 276 9694'
+    },
+    {
+      'name': 'Gallo Negro',
+      'picture': 'images/rapida/gallo.jpg',
+      'direccion': 'Carlos Condell 219, Rengo, VI Región',
+      'telefono': '+56 9 9694 4988'
+    },
+    {
+      'name': 'Pizzería La Mazza',
+      'picture': 'images/rapida/lamazza.jpg',
+      'direccion': 'José Bisquert 357, Rengo, VI Región',
+      'telefono': '(72) 251 5126'
+    }
+    ]
+  },
+  'bar': {
+    'datos': [{
+      'name': 'Adobee',
+      'picture': 'images/resto-bar/adobee2.jpg',
+      'direccion': 'Manuel Rodríguez 198, Rengo, VI Región',
+      'telefono': ' (72) 252 9744'
+    },
+    {
+      'name': 'El Bar del Camello',
+      'picture': 'images/resto-bar/20245768_302686143587265_2463631577984888387_n.jpg',
+      'direccion': 'Pje 0 2, Rengo, VI Región',
+      'telefono': ' +56 9 89564095'
+    },
+            
+    {
+      'name': 'D-Carlitos',
+      'picture': 'images/resto-bar/carlitos.jpg',
+      'direccion': 'Arturo Prat 503, Rengo, VI Región',
+      'telefono': ' (72) 268 1272'
+    },
+    {
+      'name': 'Sabrosisimo',
+      'picture': 'images/resto-bar/sabro.jpg',
+      'direccion': 'Urriola 119, Rengo, VI Región',
+      'telefono': ' (72) 268 1272'
+    }, 
+    ]
+  }
 };
 
 
